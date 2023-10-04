3bc3583bb8799e35183eac3e4079d1679ec5b36a
jeangalvis <campusjean2023@gmail.com>
2023-10-04 03:51:22 -0500

    feat(Extensions): :sparkles: Adding UnitOfWork to extensions

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aplicacion.UnitOfWork;
+using Dominio.Interfaces;
 
 namespace API.Extensions;
 public static class ApplicationServiceExtension
@@ -15,4 +17,9 @@ public static class ApplicationServiceExtension
            .AllowAnyHeader()
            );
        });
+    public static void AddAplicacionServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
 }
