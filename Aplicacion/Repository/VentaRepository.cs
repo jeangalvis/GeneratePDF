a9f0e18d986a53d935fb915e316aab26dee78d03
jeangalvis <campusjean2023@gmail.com>
2023-10-04 04:54:48 -0500

    feat(Repository): :sparkles: Adding function to repository

--- Aplicacion/Repository/VentaRepository.cs
+++ Aplicacion/Repository/VentaRepository.cs
@@ -1,5 +1,7 @@
 using Dominio.Entities;
 using Dominio.Interfaces;
+using Dominio.Views;
+using Microsoft.EntityFrameworkCore;
 using Persistencia;
 
 namespace Aplicacion.Repository;
@@ -9,5 +11,23 @@ public class VentaRepository : GenericRepository<Venta>, IVenta
     public VentaRepository(GeneratePdfContext context) : base(context)
     {
         _context = context;
+    }
+
+    public async Task<IEnumerable<VentasxMes>> GetVentasxMes(int anio)
+    {
+        return await (
+                from per in _context.Personas
+                join v in _context.Ventas on per.Id equals v.IdPersonafk
+                join pv in _context.ProductoVentas on v.Id equals pv.IdVentafk
+                join p in _context.Productos on pv.IdProductofk equals p.Id
+                select new VentasxMes
+                {
+                    NombreProducto = p.NombreProducto,
+                    Cantidad = pv.Cantidad,
+                    FechaVenta = v.FechaVenta,
+                    IdPersonafk = per.Id,
+                    Total = pv.Cantidad * p.Precio
+                }
+        ).ToListAsync();
     }
 }
