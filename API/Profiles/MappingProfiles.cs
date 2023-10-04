50c6f7d976f54a680f447dec92fc56b4fd8df635
jeangalvis <campusjean2023@gmail.com>
2023-10-04 05:04:10 -0500

    feat(Profiles): :sparkles: Add mapping

--- API/Profiles/MappingProfiles.cs
+++ API/Profiles/MappingProfiles.cs
@@ -1,9 +1,12 @@
+using API.Dtos;
 using AutoMapper;
+using Dominio.Views;
 
 namespace API.Profiles;
 public class MappingProfiles : Profile
 {
     protected MappingProfiles()
     {
+        CreateMap<VentasxAnio, VentasxAnioDto>().ReverseMap();
     }
 }
