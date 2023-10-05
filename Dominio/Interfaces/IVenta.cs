4689408f4efd02b3ae874e3c49071a04c26fb6cd
jeangalvis <campusjean2023@gmail.com>
2023-10-05 16:15:24 -0500

    refactor(Repository): :sparkles: Changing the function generatepdf from controller to repository

--- Dominio/Interfaces/IVenta.cs
+++ Dominio/Interfaces/IVenta.cs
@@ -1,8 +1,10 @@
 using Dominio.Entities;
 using Dominio.Views;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dominio.Interfaces;
 public interface IVenta : IGeneric<Venta>
 {
     Task<IEnumerable<VentasxAnio>> GetVentasxAnio(int anio);
+    Task<byte[]> GenPdf(IEnumerable<VentasxAnio> datos);
 }
